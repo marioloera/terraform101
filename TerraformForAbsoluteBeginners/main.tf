ababe27653699d131966ffe113962a307b144fa8
Mario Loera <mario.loera@trustly.com>
2021-12-31 10:14:53 -0700

    add new sensitive_content & file_permission

--- TerraformForAbsoluteBeginners/main.tf
+++ TerraformForAbsoluteBeginners/main.tf
@@ -1,5 +1,11 @@
 resource "local_file" "games" {
+    filename = "/Users/mario.loera/c/terraform101/TerraformForAbsoluteBeginners/fav_games.txt"
+    sensitive_content = "Fifa 2021"
+    file_permission = "0755"
+}
+
+resource "local_file" "games0" {
     content     = "Fifa 2021"
     # filename = "${path.module}/foo.bar"
-    filename = "/Users/mario.loera/c/terraform101/TerraformForAbsoluteBeginners/fav_games.txt"
+    filename = "/Users/mario.loera/c/terraform101/TerraformForAbsoluteBeginners/fav_games0.txt"
 }
