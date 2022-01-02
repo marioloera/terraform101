7647f02a1bb8f9f9844d93e766d191d9b655d177
Mario Loera <mario.loera@trustly.com>
2022-01-01 18:39:38 -0700

    add main.tf

---
+++ gcp/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("./keys/chrome-bearing-336918-09064f2caabd_terraform.json")
+
+  project = "chrome-bearing-336918"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
