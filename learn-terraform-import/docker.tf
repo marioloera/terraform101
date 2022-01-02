551e3aaf9594defe5f43b5e3a3d32981cab7097d
Mario Loera <mario.loera@trustly.com>
2022-01-02 14:21:04 -0700

    add docker_image in config and state

--- learn-terraform-import/docker.tf
+++ learn-terraform-import/docker.tf
@@ -1,3 +1,7 @@
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
 resource "docker_container" "web" {
   name  = "hashicorp-learn"
   image = "sha256:605c77e624ddb75e6110f997c58876baa13f8754486b461117934b24a9dc3a85"
