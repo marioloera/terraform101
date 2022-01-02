33472d43520e8ec63222bb413f9f270352643e39
Mario Loera <mario.loera@trustly.com>
2022-01-02 14:13:36 -0700

    add clean docker configuration but it has to recreaded

--- learn-terraform-import/docker.tf
+++ learn-terraform-import/docker.tf
@@ -1,1 +1,9 @@
-resource "docker_container" "web" {}
+resource "docker_container" "web" {
+  name  = "hashicorp-learn"
+  image = "sha256:605c77e624ddb75e6110f997c58876baa13f8754486b461117934b24a9dc3a85"
+
+  ports {
+    external = 8080
+    internal = 80
+  }
+}
