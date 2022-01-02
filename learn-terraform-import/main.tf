18068861b845772d185f2cbe848904d8369f7c37
Mario Loera <mario.loera@trustly.com>
2022-01-02 13:06:42 -0700

    fix error:   provider "docker" {

--- learn-terraform-import/main.tf
+++ learn-terraform-import/main.tf
@@ -11,6 +11,12 @@ terraform {
   required_version = ">= 0.14"
 }
 
-provider "docker" {
-  host = "npipe:////.//pipe//docker_engine"
-}
+provider "docker" {}
+# │ Error: Error initializing Docker client: protocol not available
+# │
+# │   with provider["registry.terraform.io/kreuzwerker/docker"],
+# │   on /Users/mario.loera/c/terraform101/learn-terraform-import/main.tf line 14, in provider "docker":
+# │   14: provider "docker" {
+# provider "docker" {
+#   host = "npipe:////.//pipe//docker_engine"
+# }
