885222eb4cf9fec561222f9ab4e01b8155582f83
Mario Loera <mario.loera@trustly.com>
2022-01-03 19:04:14 -0700

    add secrest for sensitve variables

--- learn-terraform-sensitive-variables/variables.tf
+++ learn-terraform-sensitive-variables/variables.tf
@@ -1,3 +1,15 @@
+variable "db_username" {
+  description = "Database administrator username"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "Database administrator password"
+  type        = string
+  sensitive   = true
+}
+
 variable "aws_region" {
   description = "AWS region"
   type        = string
