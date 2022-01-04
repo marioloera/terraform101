e6bef472ecedfe6bb8e9ce813715d3820cd84e6b
Mario Loera <mario.loera@trustly.com>
2022-01-03 22:26:43 -0700

    checkout tags/count-multiple-instances

--- learn-terraform-count-foreach/main.tf
+++ learn-terraform-count-foreach/main.tf
@@ -68,8 +68,15 @@ module "elb_http" {
   security_groups = [module.lb_security_group.this_security_group_id]
   subnets         = module.vpc.public_subnets
 
-  number_of_instances = 2
-  instances           = [aws_instance.app_a.id, aws_instance.app_b.id]
+  # The name of resources or modules provisioned with count refers to the entire collection.
+  # In this example, aws_instance.app now refers to all of the EC2 instances.
+  # You can reference individual items in collections with the same notation as list indexing.
+  # For example, aws_instance.app[0] refers to the first instance Terraform provisions.
+  number_of_instances = length(aws_instance.app)
+  # You can create a list of all of the values of a given attribute
+  # for the items in the collection with a star.
+  # For instance, aws_instance.app.*.id will be a list of all of the IDs of the instances.
+  instances = aws_instance.app.*.id
 
   listener = [{
     instance_port     = "80"
@@ -97,34 +104,13 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
-resource "aws_instance" "app_a" {
+resource "aws_instance" "app" {
+  count = var.instances_per_subnet * length(module.vpc.private_subnets)
+
   ami           = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
 
-  subnet_id              = module.vpc.private_subnets[0]
-  vpc_security_group_ids = [module.app_security_group.this_security_group_id]
-
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo yum update -y
-    sudo yum install httpd -y
-    sudo systemctl enable httpd
-    sudo systemctl start httpd
-    echo "<html><body><div>Hello, world!</div></body></html>" > /var/www/html/index.html
-    EOF
-
-  tags = {
-    Terraform   = "true"
-    Project     = var.project_name
-    Environment = var.environment
-  }
-}
-
-resource "aws_instance" "app_b" {
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = var.instance_type
-
-  subnet_id              = module.vpc.private_subnets[1]
+  subnet_id              = module.vpc.private_subnets[count.index % length(module.vpc.private_subnets)]
   vpc_security_group_ids = [module.app_security_group.this_security_group_id]
 
   user_data = <<-EOF
