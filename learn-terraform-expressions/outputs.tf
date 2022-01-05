1252fc83e8eb895184159b17684c7955235c507e
Mario Loera <mario.loera@trustly.com>
2022-01-04 19:09:50 -0700

    splat expression
    The splat expression captures all objects in a
    # list that share an attribute.

--- learn-terraform-expressions/outputs.tf
+++ learn-terraform-expressions/outputs.tf
@@ -1,3 +1,15 @@
-output "tags" {
-  value = aws_instance.ubuntu.tags
+output "first_tags" {
+  value = aws_instance.ubuntu[0].tags
+}
+
+# Your configuration contains multiple instances.
+# To return the private IP addresses Terraform creates for these instances,
+# you will implement a splat * expression as an output.
+
+# The splat expression captures all objects in a
+# list that share an attribute.
+# The special * symbol iterates over all of the elements of a given list and
+# returns information based on the shared attribute you define.
+output "private_addresses" {
+  value = aws_instance.ubuntu[*].private_dns
 }
