2296a90af9972af908f27899356bd3c503905d07
Mario Loera <mario.loera@trustly.com>
2022-02-03 15:01:45 +0100

    required_providers

--- gcp_provider/provider.tf
+++ gcp_provider/provider.tf
@@ -1,16 +1,26 @@
 terraform {
   required_version = "1.1.5"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.84.0"
+    }
+
+    google-beta = {
+      source  = "hashicorp/google"
+      version = "~> 3.84.0"
+    }
+  }
 }
 
 
 provider "google" {
   project = "trustly-data-services-prod"
   region  = "europe-west1"
-  version = "~> 3.84.0"
 }
 
 provider "google-beta" {
   project = "trustly-data-services-prod"
   region  = "europe-west1"
-  version = "~> 3.84.0"
 }
