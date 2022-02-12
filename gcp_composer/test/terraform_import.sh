d25224ec193c3b59a5c7ff01f21f07b6ba941660
Mario Loera <mario.loera@trustly.com>
2022-02-12 13:25:13 +0100

    add current policies

--- gcp_composer/test/terraform_import.sh
+++ gcp_composer/test/terraform_import.sh
@@ -10,5 +10,23 @@ terraform import google_service_account.svc_composer_worker 11130021857340304142
 terraform import google_service_account.svc_github_action_application_deployment 103303564377518452756
 
 
-# TODO how to import network and subnetwork
+# TODO how to import network and subnetwork didn't work
 terraform import google_compute_network.composer_network eyJlMmJmYjczMC1lY2FhLTExZTYtOGY4OC0zNDM2M2JjN2M0YzAiOnsiY3JlYXRlIjoyNDAwMDAwMDAwMDAsImRlbGV0ZSI6MjQwMDAwMDAwMDAwLCJ1cGRhdGUiOjI0MDAwMDAwMDAwMH19
+
+# manually delete the network from the console
+
+
+gcloud projects get-iam-policy trustly-data-services-test > iam_policy_trustly_test.yaml
+
+
+
+# roles/composer.ServiceAgentV2Ext
+option
+1) add roles/composer.ServiceAgentV2Ext to svc_composer_worker
+
+2) use terraform service account
+
+
+- members:
+  - serviceAccount:service-496466744113@cloudcomposer-accounts.iam.gserviceaccount.com
+  role: roles/composer.serviceAgent
